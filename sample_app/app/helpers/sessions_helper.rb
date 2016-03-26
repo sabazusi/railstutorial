ebd2212a720636795c2dc07b60262d9c3cb49c40
sabazusi <kouhei.live@gmail.com>
2016-03-26 10:30:48 +0900

    compl chap9

--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -24,6 +24,11 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  # 与えられたユーザーがログイン済みユーザーであればtrueを返す
+  def current_user?(user)
+    user == current_user
+  end
+
   def logged_in?
     !current_user.nil?
   end
@@ -39,6 +44,17 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  # 記憶したURL (もしくはデフォルト値) にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
   end
 
 end
--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -5,9 +5,8 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
-  validates :password, presence: true, length: { minimum: 6 }
-
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   # 与えられた文字列のハッシュ値を返す 
   def User.digest(string)
